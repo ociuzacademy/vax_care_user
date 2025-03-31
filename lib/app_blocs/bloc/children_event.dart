9755bffe11b12a5f11870181bfac99d7de14a624
Mikhil Mohan C <mikhilmohan13@gmail.com>
2025-03-31 19:21:23 +0530

    removed unwanted files

--- lib/app_blocs/bloc/children_event.dart
+++
@@ -1,7 +0,0 @@
-part of 'children_bloc.dart';
-
-@freezed
-class ChildrenEvent with _$ChildrenEvent {
-  const factory ChildrenEvent.started() = _Started;
-  const factory ChildrenEvent.childrenFetched() = _childrenFetched;
-}
